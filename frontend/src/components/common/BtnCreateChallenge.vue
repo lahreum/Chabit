ecb35cfe83b3112dac74823098a019269cbc3c98
이동훈 <cjswodlehdgn@gmail.com>
2021-01-29 15:27:40 +0900

    [S04P12B207-48]Feat: add Btn components

---
+++ frontend/src/components/common/BtnCreateChallenge.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-btn
+    class="mx-2"
+    width="60px"
+    height="60px"
+    fab
+    color="#B71C1C"
+    dark
+  >
+    <v-icon dark>
+      mdi-plus
+    </v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/common/BtnLarge.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-btn
+    class="text-center mx-auto white--text"
+    width="220px"
+    height="40px"
+    rounded
+    color="#B71C1C"
+  >
+    Log in
+  </v-btn>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
