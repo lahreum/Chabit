48ed3f210bba76bb8c936b151c18c36cf4f3b87c
이동훈 <cjswodlehdgn@gmail.com>
2021-02-02 01:08:11 +0900

    [S04P13B207-33]Feat: add PersonalChallenge.vue, PersonalChallengeList.vue, challenge.css

---
+++ frontend/src/views/challenge/PersonalChallenge.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="top-personal-challenge-type">
+      <i class="fas fa-chevron-left"></i>
+      <span>Opened</span>
+    </div>
+    <div class="challenge-card">
+      <div class="challenge-card-list">
+        <PersonalChallengeList />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./challenge.css"
+import PersonalChallengeList from "./PersonalChallengeList.vue"
+
+export default {
+  components: {
+    PersonalChallengeList
+  }
+}
+</script>
+
+<style>
+
+</style>
