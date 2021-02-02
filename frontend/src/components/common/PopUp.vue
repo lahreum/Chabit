bf3359d7ed1bc8a05e5277ba667c89a307b8b7e4
윤지해 <stanajh@naver.com>
2021-02-02 09:45:00 +0900

    [S04P13B207-23] Feat: add PopUp.vue

---
+++ frontend/src/components/common/PopUp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="popup" class="popup">
+    <v-app id="inspire">
+      <v-row justify="center">
+        <v-dialog v-model="dialog" persistent max-width="290">
+          <v-card class="rounded-xl justify-center text-sm-center">
+            <v-card-title class="headline justify-center rounded-xl">
+              <!-- <strong>{{popupTitle}}</strong> -->
+              <strong>회원가입</strong>
+            </v-card-title>
+            <!-- <v-card-text>{{popupContent}}</v-card-text> -->
+            <!-- <v-card-text class="justify-center"><strong>회원가입하시겠습니까?</strong></v-card-text> -->
+            <p>회원가입하시겠습니까?</p>
+            <v-divider></v-divider>
+            <v-card-actions class="rounded-xl">
+              <v-spacer></v-spacer>
+              <v-btn text @click="dialog = false">
+                No
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-divider vertical></v-divider>
+              <v-spacer></v-spacer>
+              <v-btn color="red darken-4" text @click="dialog = false">
+                Yes
+              </v-btn>
+              <v-spacer></v-spacer> </v-card-actions
+          ></v-card>
+        </v-dialog>
+      </v-row>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["popupTitle", "popupContent"],
+  data() {
+    return {
+      dialog: true,
+    };
+  },
+};
+</script>
