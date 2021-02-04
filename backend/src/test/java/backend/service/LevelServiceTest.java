8fbb1fd8330cc64a5a7483d9975442f53fdf9c40
김민혁 <glenn93516@gmail.com>
2021-02-04 13:55:53 +0900

    [S04P13B207-59]Fix: Test 에러 수정

--- backend/src/test/java/backend/service/LevelServiceTest.java
+++ backend/src/test/java/backend/service/LevelServiceTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class LevelServiceTest {
     @Autowired
     UserService userService;
