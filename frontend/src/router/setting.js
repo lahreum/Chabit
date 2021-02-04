31e62885fbdef70573a7e6182a21aefed1fef5e7
윤지해 <stanajh@naver.com>
2021-02-04 10:05:11 +0900

    [S04P13B207-24] remove: reflec removed pages from setting.js

--- frontend/src/router/setting.js
+++ frontend/src/router/setting.js
@@ -1,6 +1,4 @@
 import Setting from "../views/setting/Setting.vue";
-import AlarmSetting from "../views/setting/AlarmSetting.vue";
-import UserSetting from "../views/setting/UserSetting.vue";
 import ModifyUser from "../views/setting/ModifyUser.vue";
 import ManageMember from "../views/setting/ManageMember.vue";
 import MemberList from "../views/setting/MemberList.vue";
@@ -10,18 +8,6 @@ export default [
     path: "/setting",
     name: "Setting",
     component: Setting,
-    children: [
-      {
-        path: "alarm-setting",
-        name: "AlarmSetting",
-        component: AlarmSetting,
-      },
-      {
-        path: "user-setting",
-        name: "UserSetting",
-        component: UserSetting,
-      },
-    ],
   },
   {
     path: "/modify-user",
