0d29fb7ced21e8e461d1dc459299edcbd336dd12
이동훈 <cjswodlehdgn@gmail.com>
2021-02-06 19:59:56 +0900

    [S04P13B207-84]Update: update challenge page

--- frontend/src/views/challenge/Challenge.vue
+++ frontend/src/views/challenge/Challenge.vue
@@ -1,73 +1,34 @@
 <template>
-  <div class="container">
-    <!-- <div class="search-bar">
-      <input @keyup.enter="onSearch" type="search" placeholder=" Search">
+  <div>
+    <div class="search">
+      <search-bar />
       <i class="fas fa-filter"></i>
-    </div> -->
-    <!-- <SearchBar /> -->
-    <div v-if="!isTrue">
-      <div class="challenge-hot">
-        <HotChallengeList />
-      </div>
-      <div class="new-challenge">
-        <div class="new-challenge-title">
-          <span class="hot-popular">새로운</span><span>챌린지</span>
-        </div>
-        <div class="new-challenge-card-list">
-          <NewChallengeList 
-            v-for="(review, idx) in reviews"
-            :key=idx
-          />
-        </div>
-      </div>
     </div>
-    <div v-else>
-      <div class="search-result">
-        <div class="search-result-title">
-          <span>'{{title}}' 검색 결과</span>
-        </div>
-        <div class="search-result-list">
-          <!-- <SearchChallengeList /> -->
-        </div>
-      </div>
-    </div>
-    <div class="create-challenge-btn">
-      <BtnCreateChallenge />
-    </div>
+    <hot-challenge-list />
+    <new-challenge-list />
   </div>
 </template>
 
 <script>
-import BtnCreateChallenge from '../../components/common/BtnCreateChallenge.vue'
+import SearchBar from '../../components/common/SearchBar.vue'
 import HotChallengeList from './HotChallengeList.vue'
 import NewChallengeList from './NewChallengeList.vue'
-// import SearchBar from '../../components/common/SearchBar.vue'
-// import SearchChallengeList from './SearchChallengeList.vue'
-
 export default {
-  components: { 
-    HotChallengeList, 
-    NewChallengeList, 
-    BtnCreateChallenge,
-    // SearchBar,
-    // SearchChallengeList 
-  },
-  data() {
-    return {
-      reviews: [1,2,3,4,5,6,7,8],
-      isTrue: false,
-      title: ''
-    }
-  },
-  methods: {
-    onSearch(event) {
-      this.isTrue = true;
-      this.title = event.target.value;
-    }
-  }
+  components: { SearchBar, HotChallengeList, NewChallengeList },
+
 }
 </script>
 
-<style>
+<style scoped>
+.search {
+  margin: 0 1rem 0 1rem;
+  display: flex;
+  justify-content: center;
+}
 
+.search > .fa-filter {
+  font-size: 2rem;
+  margin-top: 1.2rem;
+  color: #B71C1C;
+}
 </style>
