fed2e2bb3a9438b68c46dbe0e842d663cadef04b
윤지해 <stanajh@naver.com>
2021-02-08 02:34:17 +0900

    fixed #86 Update: change format and apply vuetify Login.vue, Join.vue, FindPw, FindEmailFinish.vue, FindEmail.vue, Fine.vue, ChangePw.vue, ModifyUser.vue

--- frontend/src/views/user/ChangePw.vue
+++ frontend/src/views/user/ChangePw.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="outside">
-    <div class="logo">
-    </div>
-    <img class="chabit-logo" src="../../assets/img/chabit.jpg" alt="chabit">
+    <div class="logo"></div>
+    <img class="chabit-logo" src="../../assets/img/chabit.jpg" alt="chabit" />
     <div class="inside">
       <div class="change-password">
-        <p>Change Password</p>
-        <div class="input-with-label">
-          <input type="password" placeholder="New Password">
+        <p>비밀번호 변경</p>
+        <v-card-text class="text-input">
+          <v-text-field v-model="form.password" label="비밀번호" type="password"></v-text-field>
+          <v-text-field v-model="form.confirm" label="비밀번호 확인" type="password"></v-text-field>
+        </v-card-text>
+        <div class="bottom">
+          <button class="btn-next">
+            다음
+          </button>
         </div>
-        <hr>
-        <div class="input-with-label">
-          <input type="password" placeholder="Confirm">
-        </div>
-        <hr>
-        <button class="btn-next">
-          Next
-        </button>
       </div>
     </div>
-    <br>
+    <br />
   </div>
 </template>
 
 <script>
-import "./user.css"
+import "./user.css";
 export default {
-
-}
+  data() {
+    return {
+      form: {
+        password: "",
+        confirm: "",
+      },
+    };
+  },
+};
 </script>
-
-<style>
-
-</style>
