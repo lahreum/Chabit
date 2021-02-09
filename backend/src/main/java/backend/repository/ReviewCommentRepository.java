04b2fb89aa23addd06d6aabf772c1f8b1c6cf084
백정현 <qorwjdgus8951@gmail.com>
2021-02-09 13:41:39 +0900

    feature. ReviewCommentRepository.java saveMethod
    feature. ReviewCommentRepository.java saveMethod

---
+++ backend/src/main/java/backend/repository/ReviewCommentRepository.java
@@ -0,0 +1,18 @@
+package backend.repository;
+
+import backend.domain.ReviewComment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class ReviewCommentRepository {
+    private final EntityManager entityManager;
+
+    public ReviewComment saveReviewComment(ReviewComment reviewComment){
+        entityManager.persist(reviewComment);
+        return reviewComment;
+    }
+}
