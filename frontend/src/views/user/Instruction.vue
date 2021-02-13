26005eefd84ee2312e66d6e72076a4079192ab02
백정현 <qorwjdgus8951@gmail.com>
2021-02-14 03:02:21 +0900

    [S04P13B207-176] Update Instruction 페이지 생성
    Css 작업 + Instruction 타이틀, subtitle 작성

--- frontend/src/views/user/Instruction.vue
+++ frontend/src/views/user/Instruction.vue
@@ -1,37 +1,58 @@
 <template>
+  <div class="carousel-container">
+  <h1>환영합니다.</h1>
   <v-carousel :show-arrows="false"
           cycle
-          interval = 4000
-          height = 50vh
-          align="center"
-          justify="center"
+          interval = 3700
+          height = 55vh
+          hide-delimiter-background
   >
-    <v-carousel-item v-for="(Image,i) in Images" :key="i">
-    <div classs="instruction-Container">
-      <img id="InstructionImage" :src="Image.src"
-            align="center"
-            justify="center">
+    <v-carousel-item v-for="(Item,i) in Items" :key="i">
+    <div classs="instruction-container">
+      <img id="InstructionImage" :src="Item.src">
+      <v-list light>
+        <v-list-item>
+          <div class="item-content">
+            <div class ="item-title" v-html="Item.title"></div>
+            <div class ="item-subtitle" v-html="Item.subtitle"></div>
+          </div>
+        </v-list-item>
+      </v-list>
     </div>
     </v-carousel-item>
   </v-carousel>
+  </div>
 </template>
 
 <script>
   export default {
     data () {
       return {
-        Images: [
+        Items: [
           {
-            src: require('@/assets/img/InstructionImage/Coding.png')
+            src: require('@/assets/img/InstructionImage/Coding.png'),
+            title : "같이 공부해요!",
+            subtitle : "만약 공부를 한다면,<br>공부 챌린지에 함께 도전해요!"
           },
           {
-            src: require('@/assets/img/InstructionImage/Cooking.png')
+            src: require('@/assets/img/InstructionImage/Cooking.png'),
+            title : "취미도 공유해요!",
+            subtitle : "요리부터 미술까지!<br>다양한 취미에 참가해봐요!"
           },
           {
-            src: require('@/assets/img/InstructionImage/Yoga.png')
+            src: require('@/assets/img/InstructionImage/Yoga.png'),
+            title : "함께 운동해요!",
+            subtitle : "작심삼일 그것은 옛말입니다.<br>이제는 다른 사람과 함께 운동해봐요!"
           },
           {
-            src: require('@/assets/img/InstructionImage/HobbyCool.png')
+            src: require('@/assets/img/InstructionImage/Congratu.png'),
+            title : "함께 축하해요!",
+            subtitle : "성공 시 리뷰를 남기고,<br>다른사람과 함께 축하해봐요."
+          },
+          {
+            src: require('@/assets/img/InstructionImage/HobbyCool.png'),
+            title : "멋져요!",
+            subtitle : "다른 사람의 리뷰에 `멋져요!`를 남겨봐요.<br>동기부여가 남다르겠죠?"
           },
         ],
       }
@@ -40,13 +61,36 @@
 </script>
 
 <style>
+  h1{
+    margin-bottom : 5vh;
+  }
   #InstructionImage{
     width : 80vw;
     height: 35vh;
     max-width : 350px;
     max-heigth : 240px;
   }
-  .v-carousel {
-    height : 50vh;
+  .carousel-container{
+    width:100vw;
+    height:100vh;
+    text-align : center;
+  }
+  .item-content{
+    align-items: center;
+    align-self: center;
+    flex-wrap: wrap;
+    flex: 1 1;
+    overflow: hidden;
+  }
+  .item-title{
+      color:black;
+      font-size : 2.7vh;
+      font-weight: 600;
+      margin-bottom : 1vh;
+  }
+  .item-subtitle{
+    color : rgba(0, 0, 0, 0.6);
+    font-size : 2.1vh;
+    font-weight : 500;
   }
 </style>
