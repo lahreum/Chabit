4c6d740c02896108ab30a484f0b3aad0532f904b
윤지해 <stanajh@naver.com>
2021-02-18 19:04:52 +0900

    [S04P13B207-269] Bug: ManageMember.vue-회원정보 중복시 초기 화면 로드 문제 및 검색결과 확인 안되는 부분 수정

--- frontend/src/views/setting/ManageMember.vue
+++ frontend/src/views/setting/ManageMember.vue
@@ -1,129 +1,134 @@
 <template>
-<sequential-entrance fromBottom> 
-  <v-card class="manage-member" flat>
-    <v-card-title color="white">
-      <v-spacer></v-spacer>
-      <v-autocomplete
-        v-model="search"
-        append
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-no-data
-        hide-details
-        clearable
-        item-value="userName"
-        item-text="userName"
+  <sequential-entrance fromBottom>
+    <v-card class="manage-member" flat>
+      <v-card-title color="white">
+        <v-spacer></v-spacer>
+        <v-autocomplete
+          v-model="search"
+          append
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-no-data
+          hide-details
+          clearable
+          item-value="userName"
+          item-text="userName"
+          :items="memberInfo"
+          :menu-props="{
+            top: false,
+            bottom: true,
+            offsetY: true,
+            offsetX: true,
+          }"
+        ></v-autocomplete>
+        <v-spacer></v-spacer>
+        <v-btn
+          elevation="1"
+          rounded
+          plain
+          depressed
+          color="error"
+          @click="dialog = !dialog"
+        >
+          Forced</v-btn
+        >
+        <!-- Start Popup dialog -->
+        <p v-if="dialog">
+          <v-row justify="center">
+            <v-dialog
+              v-model="dialog"
+              persistent
+              max-width="290"
+              content-class="rounded-xl"
+            >
+              <v-card class="rounded-xl justify-center text-sm-center">
+                <v-card-title class="headline justify-center rounded-xl">
+                  <strong>회원삭제</strong>
+                </v-card-title>
+                <v-card-text>강제로 회원을 탈퇴시키겠습니까?</v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions class="rounded-xl">
+                  <v-spacer></v-spacer>
+                  <v-btn text @click="dialog = false"> 취소 </v-btn>
+                  <v-spacer></v-spacer>
+                  <v-divider vertical></v-divider>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="red darken-4"
+                    plain
+                    :ripple="false"
+                    text
+                    @click="deleteUserForced()"
+                  >
+                    탈퇴
+                  </v-btn>
+                  <v-spacer></v-spacer> </v-card-actions
+              ></v-card>
+            </v-dialog>
+          </v-row>
+        </p>
+        <!-- End Popup dialog -->
+        <!-- Start AfterPopup -->
+        <p v-if="afterDialog">
+          <v-row justify="center">
+            <v-dialog
+              v-model="afterDialog"
+              persistent
+              max-width="290"
+              content-class="rounded-xl"
+            >
+              <v-card class="rounded-xl justify-center text-sm-center">
+                <v-card-title class="headline justify-center rounded-xl">
+                  <strong>회원삭제 결과</strong>
+                </v-card-title>
+                <v-card-text>{{ this.afterMessage }}</v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions class="rounded-xl">
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="red darken-4"
+                    plain
+                    :ripple="false"
+                    text
+                    @click="afterDialog = !afterDialog"
+                  >
+                    확인
+                  </v-btn>
+                  <v-spacer></v-spacer> </v-card-actions
+              ></v-card>
+            </v-dialog>
+          </v-row>
+        </p>
+        <!-- End AfterPopup -->
+      </v-card-title>
+      <v-data-table
+        v-model="selected"
+        :headers="headers"
         :items="memberInfo"
-        :menu-props="{ top: false, bottom: true, offsetY: true, offsetX: true }"
-      ></v-autocomplete>
-      <v-spacer></v-spacer>
-      <v-btn
-        elevation="1"
-        rounded
-        plain
-        depressed
-        color="error"
-        @click="dialog = !dialog"
+        item-key="userNickname"
+        show-select
+        single-select
+        sort
+        :search="search"
+        class="elevation-1"
+        disable-pagination
+        hide-default-footer
       >
-        Forced</v-btn
-      >
-      <!-- Start Popup dialog -->
-      <p v-if="dialog">
-        <v-row justify="center">
-          <v-dialog
-            v-model="dialog"
-            persistent
-            max-width="290"
-            content-class="rounded-xl"
-          >
-            <v-card class="rounded-xl justify-center text-sm-center">
-              <v-card-title class="headline justify-center rounded-xl">
-                <strong>회원삭제</strong>
-              </v-card-title>
-              <v-card-text>강제로 회원을 탈퇴시키겠습니까?</v-card-text>
-              <v-divider></v-divider>
-              <v-card-actions class="rounded-xl">
-                <v-spacer></v-spacer>
-                <v-btn text @click="dialog = false"> 취소 </v-btn>
-                <v-spacer></v-spacer>
-                <v-divider vertical></v-divider>
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="red darken-4"
-                  plain
-                  :ripple="false"
-                  text
-                  @click="deleteUserForced()"
-                >
-                  탈퇴
-                </v-btn>
-                <v-spacer></v-spacer> </v-card-actions
-            ></v-card>
-          </v-dialog>
-        </v-row>
-      </p>
-      <!-- End Popup dialog -->
-      <!-- Start AfterPopup -->
-      <p v-if="afterDialog">
-        <v-row justify="center">
-          <v-dialog
-            v-model="afterDialog"
-            persistent
-            max-width="290"
-            content-class="rounded-xl"
-          >
-            <v-card class="rounded-xl justify-center text-sm-center">
-              <v-card-title class="headline justify-center rounded-xl">
-                <strong>회원삭제 결과</strong>
-              </v-card-title>
-              <v-card-text>{{ this.afterMessage }}</v-card-text>
-              <v-divider></v-divider>
-              <v-card-actions class="rounded-xl">
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="red darken-4"
-                  plain
-                  :ripple="false"
-                  text
-                  @click="afterDialog = !afterDialog"
-                >
-                  확인
-                </v-btn>
-                <v-spacer></v-spacer> </v-card-actions
-            ></v-card>
-          </v-dialog>
-        </v-row>
-      </p>
-      <!-- End AfterPopup -->
-    </v-card-title>
-    <v-data-table
-      v-model="selected"
-      :headers="headers"
-      :items="memberInfo"
-      item-key="userName"
-      show-select
-      single-select
-      sort
-      :search="search"
-      class="elevation-1"
-      disable-pagination="true"
-      hide-default-footer
-    >
-      <template
-        v-slot:[`item.data-table-select`]="{ item, isSelected, select }"
-      >
-        <v-simple-checkbox
-          v-ripple
-          :value="isSelected"
-          :readonly="checkRole(item.userRole)"
-          :disabled="checkRole(item.userRole)"
-          @input="select($event)"
-        ></v-simple-checkbox>
-      </template>
-    </v-data-table>
-  </v-card>
-  </sequential-entrance> 
+        <template
+          v-slot:[`item.data-table-select`]="{ item, isSelected, select }"
+        >
+          <v-simple-checkbox
+            v-ripple
+            :value="isSelected"
+            :readonly="checkRole(item.userRole)"
+            :disabled="checkRole(item.userRole)"
+            @input="select($event)"
+          ></v-simple-checkbox>
+        </template>
+      </v-data-table>
+    </v-card>
+  </sequential-entrance>
 </template>
 
 <script>
